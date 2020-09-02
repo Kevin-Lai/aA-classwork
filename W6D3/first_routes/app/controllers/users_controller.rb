72c8a4abd4f95dbdb53b296a75aa84fa1660e9cd
Kevin-Lai <kevinlai249@gmail.com>
2020-09-02 16:47:42 -0700

    Added W6D3 classwork

---
+++ W6D3/first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    def index
+        render json: params
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+          render json: user
+        else
+          render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+end
