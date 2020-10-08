dedf260fc87f07486fcfe72aff1570168fc8a9d5
Kevin-Lai <kevinlai249@gmail.com>
2020-10-07 23:03:32 -0700

    Added W11D3 classwork

---
+++ W11D3/BenchBnB/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to:'static_pages#root'
+  #look for root method in the static pages controller
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ W11D3/BenchBnB/frontend/bench_bnb.jsx
@@ -0,0 +1,20 @@
+// THIS IS OUT ENTRY FILE :D
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//eventlistener, waits for the event
+//once that event happens, it invokes the cb function to do its job
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Hello World</h1>, root);
+    //this first argumentm is a React component, which is why we're inporting React in line 3
+})
+
+const user = {id: 1, name: "Cat"}
+data: {user}
+
+data: {user: {id: 1, name: "Cat"}}
+
+user : user,
