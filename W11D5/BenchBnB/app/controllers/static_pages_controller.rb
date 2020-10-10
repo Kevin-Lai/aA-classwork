162b193531501b6f0e1f1ec0c8fafc2d95513561
Kevin-Lai <kevinlai249@gmail.com>
2020-10-09 18:20:09 -0700

    Added W11D5 classwork

---
+++ W11D5/BenchBnB/app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+
+  def root
+    
+    #instance method
+    #default is a a render
+    #renders a view of its own name >> try to render a files called "root" <-- find file under the /static_pages/...
+
+  end
+end
